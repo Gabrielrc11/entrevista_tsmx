e7f1323da5953f8281e4f610a4e2ea69ae785797
gabrielrc11 <gabrielhenriquerc11@gmail.com>
2025-04-25 15:07:25 -0300

    feat: Carregar arquivo

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import psycopg2
 from psycopg2 import OperationalError
+import pandas as pd
 
 def connect_db():
     # Configurações de conexão
@@ -18,8 +19,22 @@ def connect_db():
         connect.close()
     
     except OperationalError as e:
+        # Captura de erro de conexão
         print(f"Erro ao conectar ao PostgreSQL: {e}")
+
+def load_file(file_path):
+    # Função para carregar um arquivo XLSX
+    try:
+        df = pd.read_excel(file_path)
+        print(f"Arquivo {file_path} carregado com sucesso!")
+        print(f"Dimensões: {df.shape[0]} linhas x {df.shape[1]} colunas")
+        return df
+    except Exception as e:
+        print(f"Erro ao carregar o arquivo XLSX: {e}")
+        return None
 
 # Executar o teste de conexão
 if __name__ == "__main__":
     connect_db()
+    file_path = "./src/dados_importacao.xlsx"
+    df = load_file(file_path)
